d4747ab5701e48681eb5ef678054a1c397c1f1ad
Syaqinah Safar <syaqinahsafar23@gmail.com>
2019-12-25 11:01:36 +0800

    add admin function

--- application/models/Shop_model.php
+++ application/models/Shop_model.php
@@ -5,4 +5,16 @@ class Shop_model extends CI_model {
     {
         return $this->db->get('shop')->result_array();
     }
+
+    public function addProduct(){
+        $data = [
+            "name" => $this->input->post('name', true),
+            "image" => $this->input->post('image', true),
+            "category" => $this->input->post('category', true),
+            "description" => $this->input->post('description', true),
+            "price" => $this->input->post('price', true),
+            "stock" => $this->input->post('stock', true)
+        ];
+        $this->db->inseer('shop', $data);
+    }
 }
