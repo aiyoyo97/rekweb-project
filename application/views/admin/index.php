a7e82d0d5635868f56550cccc9dd0c2a4114f1c9
Syaqinah Safar <syaqinahsafar23@gmail.com>
2019-12-25 11:03:29 +0800

    main frame for admin

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -1,4 +1,6 @@
 <div class="container">
+
+
         <div class="row">
             <div class="col-12">
                 <h1 class="mt-3">Product List</h1>
@@ -18,26 +20,57 @@
                     </div>
                 </div>
 
-                <div class="row mt-3">
-                <div class="col-md-3">
-                <?php if( empty($shop) ) : ?>
-                        <div class="alert alert-danger" role="alert">
-                    Data is not found. 
+
+                <div class="row">
+                    <div class="col-md">
+                        <table class="table table-striped table-bordered">
+                            <thead class="thead-dark md-10">
+                                <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Name</th>
+                                <th scope="col">Image</th>
+                                <th scope="col">Category</th>
+                                <th scope="col">Description</th>
+                                <th scope="col">Price</th>
+                                <th scope="col">Stock</th>
+                                <th scope="col">Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+
+                            <div class="row mt-3">
+                            <div class="col-md-3">
+                            <?php if(empty($shop)) : ?>
+                                <tr>
+                                <td colspan="4">
+                                <div class="alert alert-danger" role="alert">
+                                    Data not found!
+                                </div>
+                                </td>
+                                </tr>
+                            <?php endif; ?>
+
+                            <?php foreach ($shop as $sh) : ?>
+                                <tr>
+                                <th scope="row">1</th>
+                                <td><?= $sh['name'] ?></td>
+                                <td><?= $sh['image'] ?></td>
+                                <td><?= $sh['category'] ?></td>
+                                <td><?= $sh['description'] ?></td>
+                                <td><?= $sh['price'] ?></td>
+                                <td><?= $sh['stock'] ?></td>
+                                <td>
+                                    
+                                    <a href="<?= base_url(); ?>admin/edit/<?= $sh['id'] ?>" class="badge badge-info float-right">edit</a>
+
+                                    <a href="<?= base_url(); ?>admin/delete/<?= $sh['id'] ?>" class="badge badge-danger float-right button-delete">delete</a>
+                                </td>
+                                </tr>
+                            <?php endforeach; ?>
+                            </tbody>
+                        </table>
                     </div>
-                <?php endif; ?>
-
-                <ul class="list-group">
-                    <?php foreach( $shop as $sh ) : ?>
-                        <li class="list-group-item"><?= $sh['name'] ?>
-                        <a href="<?= base_url(); ?>admin/delete/<?= $sh['id'] ?>" class="badge badge-danger float-right button-delete">delete</a>
-
-                        <a href="<?= base_url(); ?>admin/edit/<?= $sh['id'] ?>" class="badge badge-success float-right">edit</a>
-
-                        <a href="<?= base_url(); ?>admin/details/<?= $sh['id'] ?>" class="badge badge-primary float-right">details</a>
-                    </li>
-                    <?php endforeach; ?>
-                </ul>
-
+              </div>
             </div>
         </div>
     </div>
